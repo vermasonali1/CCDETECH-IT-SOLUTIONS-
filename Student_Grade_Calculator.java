5755f1ecb1f7af7107d8432749712cc0f38d3907
Sonali Verma <120965928+vermasonali1@users.noreply.github.com>
2023-12-30 19:05:19 +0530

    Student_Grade_Calculator.

--- Student_Grade_Calculator.java
+++ Student_Grade_Calculator.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class Student_Grade_Calculator {
     public static void main(String[] args) {
 
+        
         // Take input form user
         Scanner s=new Scanner(System.in);
         System.out.print("Enter Marks subject_1: ");
